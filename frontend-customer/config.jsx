b2d2d2e1b3293d8ea25b0c293e5f1a62a75fc5a3
21522604 <21522604@gm.uit.edu.vn>
2024-06-19 20:34:17 +0700

    configure js

--- frontend-customer/config.jsx
+++ frontend-customer/config.jsx
@@ -1,2 +1,2 @@
-export const backendAPI = 'http://44.200.152.53:8080'
-export const frontendAPI = 'http://35.153.39.0:3000'
+export const backendAPI = 'http://3.236.125.29:8080'
+export const frontendAPI = 'http://54.165.217.141:3000'
